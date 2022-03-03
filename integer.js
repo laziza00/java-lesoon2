991bcd158195f37ad7ab5d53c7a45d54cf2f2dea
laziza@mail.ru <laziza00>
2022-03-03 09:56:50 +0500

    initial commit

---
+++ integer.js
@@ -0,0 +1,554 @@
+
+// if 1 masala
+
+// let a = +prompt("a: ");
+// let b = +prompt("b: ");
+// let c = +prompt("c: ");
+
+// if (a<b && a<c) {
+//     console.log(a);
+// }
+// else if (b<a && b<c) {
+//     console.log(b);
+// }
+// else {
+//     console.log(c);
+// }
+
+// if 2 masala
+
+// let a = +prompt("a: ");
+// let b = +prompt("b: ");
+// let c = +prompt("c: ");
+
+// if (a<b && a>c) {
+//     console.log(a);
+// }
+// else if (b<a && b>c) {
+//     console.log(b);
+// }
+// else {
+//     console.log(c);
+// }
+
+
+// if 3 masala
+
+// let a = +prompt("a: ");
+// let b = +prompt("b: ");
+
+// if (a!=b && a>b) {
+//     console.log(a);
+// }
+// else if (b!=a && b>a) {
+//     console.log(b);
+// }
+// else if (a==b) {
+//     console.log(0);
+// }
+
+// if 4 masala
+
+// let a = +prompt("a: ");
+// let b = +prompt("b: ");
+
+// if (a> 0 && b>0) {
+//     console.log("I chorak");
+// }
+// else if (a< 0 && b> 0) {
+//     console.log("II chorak");
+// }
+// else if (a<0 && b< 0) {
+//     console.log("III chorak");
+// }
+// else {
+//     console.log("IV chorak");
+// }
+
+// if 5 masala
+
+// let a = +prompt("a: ")
+
+// let fx;
+// if (a> 0) {
+//     fx = 2* Math.sin(a)
+// }
+// else if (a<=0) {
+//     fx = a -6
+// }
+// console.log(fx);
+
+// if 6 masala chala
+
+// let a = +prompt("a: ")
+
+// let fx;
+// if (a <-2 && a>2) {
+//     fx = a*2;
+// }
+// else {
+//     fx = -3
+// }
+// console.log(fx);
+
+// if 7 masla
+
+// let x = +prompt()
+// let fx;
+// if (x <= 0) {
+//     fx = -x
+// }
+// else if (x>0 && x<2){
+//     fx = x**2
+// }
+// else {
+//     fx = 4
+// }
+// console.log(fx);
+
+// if 8 maslaa  chala
+
+// let a = +prompt("a: ");
+// let b = +prompt("b: ");
+// let c = +prompt("c: ");
+
+// if (a+b > c) {
+//     console.log("yes");
+// }
+// else {
+//     console.log("no");
+// }
+
+// if 9 maslaa
+
+
+// let x = +prompt()
+//  if (x< 0) {
+//      console.log("freezing");
+//  }
+//  else if (x >= 1 && x<=10) {
+//     console.log("very cold");
+//  }
+//  else if (x >= 11 && x<=20) {
+//     console.log(" cold");
+//  }
+//  else if (x >= 21 && x<=30) {
+//     console.log("normal");
+//  }
+//  else if (x >= 31 && x<=40) {
+//     console.log("hot");
+//  }
+//  else {
+//     console.log("very hot");
+//  }
+
+// if 10-masla
+// let a = +prompt();
+// if (a>0 && a%2==0) {
+//     console.log("juft musbat son");
+// }
+// else if(a>0 && a%2==1) {
+//     console.log("toq musbat son");
+// }
+// else if (a<0 && a%2==0) {
+//     console.log("juft manfiy son");
+// }
+// else if (a<0 && a%2==1) {
+//     console.log("toq manfiy son");
+// }
+// else if (a==0) {
+//     console.log("son 0 ga teng");
+// }
+
+// if 11 masala
+// let a = +prompt();
+// if (a%2== 0 && a <100){
+//     console.log("juft 2 xonali");
+// }
+// else if (a%2== 1 && a<100){
+//     console.log("toq 2 xonali");
+// }
+// else if (a%2== 0 && a<1000){
+//     console.log("juft 3 xonali");
+// }
+// else if (a%2== 1 && a<1000){
+//     console.log("toq 3 xonali");
+// }
+
+
+
+
+
+
+// for 17 masala
+
+// let n= 10;
+// let a= 2;
+// let s= 0;
+
+// for (let i=0; i<= n; i++){
+//     s+= a**i;
+//     // console.log(a**i);
+// }
+// console.log(s);
+
+
+
+
+
+
+
+
+
+// if 13 masala
+
+// let a= +prompt();
+// let b= prompt("tannafusdamisiz? ")
+// if (a>=20 && b == "ha"){
+//     console.log("true");
+// }
+// else {
+//     console.log("false");
+// }
+
+// if 14 masla
+
+// let a = +prompt("2 xonali son kiriting: ")
+// let b = Math.floor((a/10)%10);
+// let c = Math.floor(a%10);
+// let d = (c*10) + b;
+// console.log(d);
+// if (d<=a) {
+//     console.log("True");
+// }
+// else {
+//     console.log("False");
+// }
+
+
+
+
+
+
+// if 15 masala
+
+// let a = +prompt("100000 gacha son kiriting: ")
+// if (a <10) {
+//     console.log("1");
+// }
+// else if (a>=10 && a<100) {
+//     console.log(2);
+// }
+// else if (a>=100 && a<1000) {
+//     console.log(3);
+// }
+// else if (a>=1000 && a<10000) {
+//     console.log(4);
+// }
+// else if (a>=10000 && a<100000) {
+//     console.log(5);
+// }
+// else {
+//     console.log("6 xonali yoki undan sondan katta sonlar");
+// }
+
+
+
+// if 16 masala
+
+//  let a = +prompt('0 dan katta va 10000 dan kichik son kiriting: ');
+//  let i =  Math.floor(a /1000);
+//  let b = Math.floor(a /100)%10;
+//  let c = Math.floor((a /10)%10);
+//  let d = Math.floor(a%10);
+//  let e = i + b + c + d;
+//  console.log(e);
+
+// if 17  masala
+
+//  let a = +prompt('0 dan katta va 10000 dan kichik son kiriting: ');
+//  let i =  Math.floor(a /1000);
+//  let b = Math.floor(a /100)%10;
+//  let c = Math.floor((a /10)%10);
+//  let d = Math.floor(a%10);
+//  let e = 0;
+//  if (i%2==1 ){
+//      e += i;
+//  } 
+//  else if (b %2==1 ){
+//     e += b;
+//  }
+//  else if (c %2==1  ){
+//     e += c;
+//  }
+//  else if (d%2==1 ){
+//     e += d;
+//  }
+//   console.log(e);
+
+
+
+
+
+
+
+
+// for 1 masala
+
+// let n = 8;
+// let k = 7;
+
+// for (i = 0; i<k; i++) {
+//     console.log(n);
+// }
+
+// for 2 masala
+
+// let a = 0;
+// let b = 10;
+// count = 0
+// for (i = 10; i>=a; i--){
+//     console.log(i);
+//     count+=1;
+// }
+// console.log("sanoq: ",count);
+
+// for 3 masala
+
+// let a = 1;
+// let b = 10;
+// count = 0
+// for (i = 10-1; i>a; i--){
+//     console.log(i);
+//     count+=1;
+// }
+// console.log("sanoq: ",count);
+
+// for 4 masala
+
+// let a= 1200;
+// for (let i =1; i<=10; i+=2){
+//     console.log(`${i} kg konfetning narxi ${i*a} som`);
+// }
+
+
+// for 5 masala
+
+// let a= 1200;
+// for (let i =0.1; i<=1; i+=0.1){
+//     console.log(`${i} kg konfetning narxi ${i*a} som`);
+// }
+
+
+// for 6 masala
+
+// let a= 12500;
+// for (let i =1.2; i<=2; i+=0.2){
+//     console.log(`${i} kg konfetning narxi ${i*a} som`);
+// }
+
+// for 7 masala
+
+// let a= 0;
+// let b =20;
+// let sum = 0;
+
+// for (let i =a; i<b; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+
+// for 8 masala
+
+// let a= 1;
+// let b =10;
+// let sum = 1;
+
+
+// for (let i =a; i<b; i++) {
+//     sum *= i;
+// }
+// console.log(sum);
+
+// for 9 masala
+
+// let a= 1;
+// let b =10;
+// let sum = 0;
+
+// for (let i =a; i<b; i++) {
+//     sum += (i**2);
+// }
+// console.log(sum);
+
+// for 10 masala
+
+// let a= 1;
+// let b =50;
+// let sum = 0;
+
+// for (let i =a; i<=b; i++) {
+//     sum += (1/i);
+// }
+// console.log(sum);
+
+// for 11 masala
+
+// let n = 5;
+// let S = (n**2);
+// for (let i = 1; i < (n**2); i++) {
+//     S += (n+i)**2; 
+// };
+// console.log(S);
+// for 12 masala
+
+// let a = 2;
+// sum = 1
+// for (let i = 1; i<=a; i+=0.1){
+//     sum *= i;
+// }
+// console.log(sum.toFixed(2));
+
+
+// for 13 masala
+
+// let a =1;
+// let sum = 10;
+
+// for (i =a; i<sum; i+= 0.1) {
+//     if (i % 2==1)
+//     sum += i;
+//     else {
+//         sum -= i;
+//     }
+// }
+// console.log(sum);
+
+// 12- masala 
+
+// let n = 2;
+// let summa = 1;
+
+// for (let i = 1.1; i <= n; i += 0.1) {
+//     console.log(i.toFixed(1));
+//     summa *= i;
+// }
+// console.log(summa.toFixed(2));
+
+// 13-masala
+
+// let a = 2;
+// let sum = 0;
+// let first = a - 1;
+// for (let i = 1; i <= 10; i++) {
+//     first+=0.1;
+//     (i % 2 == 0) ? sum += first : sum -= first;
+// }
+// console.log(sum.toFixed(1));
+
+
+// 14-masala
+
+
+// let n = 5;
+
+// for (let i = 1; i < (2*n); i += 2){
+//     console.log(i*i);
+// }
+
+
+
+
+
+
+
+
+// 11-masala
+
+// let n = 5;
+// let S = (n**2);
+// for (let i = 1; i < (n**2); i++) {
+//     S += (n+i)**2; 
+// };
+// console.log(S);
+
+// 12- masala 
+
+// let n = 2;
+// let summa = 1;
+
+// for (let i = 1.1; i <= n; i += 0.1) {
+//     console.log(i.toFixed(1));
+//     summa *= i;
+// }
+// console.log(summa.toFixed(2));
+
+// 13-masala
+
+// let a = 2;
+// let sum = 0;
+// let first = a - 1;
+// for (let i = 1; i <= 10; i++) {
+//     first+=0.1;
+//     (i % 2 == 0) ? sum += first : sum -= first;
+// }
+// console.log(sum.toFixed(1));
+
+
+// 14-masala
+
+
+// let n = 5;
+
+// for (let i = 1; i < (2*n); i += 2){
+//     console.log(i*i);
+// }
+
+// for 15 masala
+
+// let a= 2;
+// let n = 7;
+// sum = 1;
+// for (let i =a; i<=a**n; a++){
+//     sum = a*a;
+// }
+// console.log(a);
+
+// for 16 masala
+
+// let a = 2;
+// let n = 5;
+// sum = 1
+// for (let i =1; i<=n; i++) {
+//     sum = (a**i);
+//     console.log(sum);
+// }
+
+// for 17 masala
+
+// let a = 2;
+// let n = 5;
+// sum = 1;
+// box = 0;
+// for (let i =1; i<=n; i++) {
+//     sum = (a**i);
+//     box += (a**i)
+//     console.log(sum);
+// }
+// console.log("yigindi",box);
+
+
+// for 18 masala
+
+// let a = 2;
+// let n = 5;
+// sum = 1;
+// box = 0;
+// for (let i =1; i<=a**n; i++) {
+//     sum = (a**i);
+//     box += (a**i)
+//     console.log(sum);
+// }
+// console.log("yigindi",box);
+
+
